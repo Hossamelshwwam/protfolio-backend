3e8dd67d914012aa98f652c2df8c19ac774a8aa3
Hossamelshwwam <h.elshwwam123@gmail.com>
2026-03-09 03:19:48 +0200

    refactor: move server entry point to `src/api/index.ts` and update build scripts in `package.json`.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import createApp from "./app";
-import connectDB from "./config/db";
+import createApp from "../app";
+import connectDB from "../config/db";
 
 const PORT = process.env.PORT || 5000;
 
